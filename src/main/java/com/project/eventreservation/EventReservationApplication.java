08819e86e2ff028f9b294db5dfdebdc4957d5e52
shahbazkhan90 <shahbazkhan00945@gmail.com>
2026-08-12 09:19:00 +0530

    initial project structure

---
+++ src/main/java/com/project/eventreservation/EventReservationApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventReservationApplication.class, args);
+    }
+
+}
